e01f807a7181d59b6ed9656e0a072e2a9f554870
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-03-31 18:58:31 +0800

    Add files via upload

---
+++ Creation of database/DatabaseCreator.py
@@ -0,0 +1,3 @@
+import os
+os.system('sql_database.py')
+os.system('csv_reader.py')
---
+++ Creation of database/sql_database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+def table_create():
+    conn = sqlite3.connect('credit_scores.db')
+    c = conn.cursor()
+
+    # Create table - STUDENTS
+    c.execute('''CREATE TABLE STUDENTS
+                     ([Generated_ID] INTEGER PRIMARY KEY,[Student_Name] text, [Register_number] integer, [Class] text)''')
+
+    # Create table - SCORES
+    c.execute('''CREATE TABLE SCORES
+                     ([Generated_ID] INTEGER PRIMARY KEY,[Register_Number] integer, [Credit_Score] text)''')
+    conn.commit()
+
+
+if __name__ == '__main__':
+    table_create()

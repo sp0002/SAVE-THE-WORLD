861241b6e8e910102270e267cc0e89e9bb14cb2b
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-03-31 18:03:11 +0800

    Add files via upload

---
+++ csv reader.py
@@ -0,0 +1,19 @@
+import csv
+import sqlite3
+conn = sqlite3.connect('credit_scores.db')
+c = conn.cursor()
+
+
+reader = csv.DictReader(open('Demo_Class_list.csv', 'rt'))
+dict_list = []
+count = 0
+for line in reader:
+    count = count + 1
+    dict_list.append(line)
+for i in range(count):
+    list1 = list(dict_list[i].values())
+    sql = "INSERT INTO STUDENTS (Student_Name, Register_Number, Class) VALUES ('%s', '%s', '%s')" % (list1[0], list1[1], list1[2])
+    c.execute(sql)
+
+
+conn.commit()
